fd35639ab7ed0cb97cb3b6d041071fc1b4998ccf
szilardguti <szilardguti@gmail.com>
2024-04-02 20:24:00 +0200

    Add backend project init

---
+++ backend/segitseg-osszesito-rendszer/src/main/java/hu/unideb/inf/segitsegosszesitorendszer/SegitsegOsszesitoRendszerApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.segitsegosszesitorendszer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegitsegOsszesitoRendszerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegitsegOsszesitoRendszerApplication.class, args);
+	}
+
+}
